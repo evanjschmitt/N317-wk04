c152c4d37f2dfdf66692e773501ae56f9677a2b7
evanjschmitt <evanjschmitt@gmail.com>
2024-09-18 11:40:06 -0400

    Almost Works (Search Funct. Broken)

--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -19,22 +19,41 @@ export default function Pokemon() {
     /**
      * @type {[pokemonApiObject, function]}
      */
-    const [pokemon, setPokemon] =useState({});
+    const [pokemon, setPokemon] =useState({ sprites: {}});
     /**
      * @type {[String, Function]}
      */
     const [searchTerm, setSearchTerm] =useState("");
     function changeSearchTerm(e) {
-        setSearchTerm(e.currentTarget.value)
+        setSearchTerm(e.currentTarget.value.toLowerCase())
     }
+
+async function searchPokemonName() {
+console.log('search is searching')
+    try {
+        setPokemon(pokeDataFormatted);
+    } catch (error) {
+        setPokemon({name: searchTerm, sprites: {}});
+    }
+
+    const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+    const pokeDataFormatted = await rawData.json();
+    console.log(dataFormatted);
+}
+
 
     return(<main>
         <main>
              <h1>Pokemon Page</h1>
              <div className={pokeStyles.search}>
                 <input type="search" id="search" name="search" value={searchTerm} onChange={changeSearchTerm}></input>
-                <input name="search"type="button" value="search"></input>
-             </div>
+                <input name="search"type="button" value="search" onClick={searchPokemonName}></input>
+                </div>
+                <h3>
+                    {pokemon.name}
+                </h3>
+                <img src={pokemon.sprites.font-default}/>
+             
         </main>
        
 
