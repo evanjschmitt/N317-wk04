bd443357d320017acf362115812b0ebc1f9b619a
evanjschmitt <evanjschmitt@gmail.com>
2024-09-25 09:57:20 -0400

    Week05: Fixed Functions (I hope)

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,35 @@
-
 "use client";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  // let score=0;
+  const [score, setScore] = useState(1);
+  const [pokemon, setPokemon] = useState({ id: 1 }); // Example state for pokemon
 
-// let score=0;
-const [score, setScore] = useState(1);
+  function addToScore() {
+    setScore(score + 1);
+  }
 
-// console.log(score);
+  // Define a function to fetch Pokemon data
+  function fetchPokemonData() {
+    if (pokemon.id) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}/encounters`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Encounter data:", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }
 
-function addToScore() {
-  // score++;
-  setScore(score + 1);
-}
+  useEffect(() => {
+    fetchPokemonData();
+  }, [pokemon.id]); // Run the effect whenever the pokemon id changes
 
   return (
     <main>
-  
       <h1>Home</h1>
       <div>
         <button onClick={addToScore}>Add 1</button>
--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -1,61 +1,68 @@
 "use client";
+
+import pokeStyles from "./pokemon.module.css";
 import { useState } from "react";
-import pokeStyles from "./pokemon.module.css"
 
-
-//Pokemon Data
+//Pokemon data
 /**
- * @typedef {Object} pokemonApiObject
- * @description This is the object for a Pokemon 
- * @prop {String} name Name of Pokemon
- * @prop {Object} sprites Object w all sprite refs
- * @prop {String} sprites.front_default default sprite img
- * @prop {Number} height Height of Pokemon (mult by 10 to make cm)
- * @prop {Number} weight Weight of Pokemon (div by 10 to make kg)
+ * @typedef {Object} pokemonApiObject This is the object for a pokemon
+ * @prop {String} name Name of pokemon
+ * @prop {Number} id id of pokemon
+ * @prop {Object} sprites Object with all sprite references
+ * @prop {String} sprites.front_default Default front image for sprite
+ * @prop {Number} height Height of pokemon. Multiply by 10 make to centimeters.
+ * @prop {Number} weight Weight of pokemon. Divide by 10 to make kilograms.
  */
 
-
 export default function Pokemon() {
-    /**
-     * @type {[pokemonApiObject, function]}
-     */
-    const [pokemon, setPokemon] =useState({ sprites: {}});
-    /**
-     * @type {[String, Function]}
-     */
-    const [searchTerm, setSearchTerm] =useState("");
-    function changeSearchTerm(e) {
-        setSearchTerm(e.currentTarget.value.toLowerCase())
-    }
+  /**
+   * @type {[pokemonApiObject, Function]}
+   */
+  const [pokemon, setPokemon] = useState({ sprites: {} });
+  /**
+   * @type {[String, Function]}
+   */
+  const [searchTerm, setSearchTerm] = useState("");
 
-async function searchPokemonName() {
-console.log('search is searching')
-    try {
+  function changeSearchTerm(e) {
+    setSearchTerm(e.currentTarget.value.toLowerCase());
+
+    async function searchForPokemonByName() {
+      try {
+        const rawData = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+        );
+        const pokeDataFormatted = await rawData.json();
         setPokemon(pokeDataFormatted);
-    } catch (error) {
-        setPokemon({name: searchTerm, sprites: {}});
+        console.log(pokeDataFormatted);
+      } catch (error) {
+        console.error("Error fetching Pokemon:", error);
+        setPokemon({ name: searchTerm, sprites: {} });
+      }
     }
 
-    const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
-    const pokeDataFormatted = await rawData.json();
-    console.log(dataFormatted);
-}
-
-
-    return(<main>
-        <main>
-             <h1>Pokemon Page</h1>
-             <div className={pokeStyles.search}>
-                <input type="search" id="search" name="search" value={searchTerm} onChange={changeSearchTerm}></input>
-                <input name="search"type="button" value="search" onClick={searchPokemonName}></input>
-                </div>
-                <h3>
-                    {pokemon.name}
-                </h3>
-                <img src={pokemon.sprites.font-default}/>
-             
-        </main>
-       
-
-    </main>)   
+    return (
+      <main>
+        <h1>Pokemon Page</h1>
+        <div className={pokeStyles.search}>
+          <input
+            type="search"
+            id="search"
+            name="search"
+            value={searchTerm}
+            onChange={changeSearchTerm} // No parentheses; you're passing a function reference
+          />
+          <input
+            type="button"
+            value="Search"
+            onClick={searchForPokemonByName} // No parentheses here as well
+          />
+        </div>
+        <h3>{pokemon.name}</h3>
+        {pokemon.sprites?.front_default && (
+          <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
+        )}
+      </main>
+    );
+  }
 }
